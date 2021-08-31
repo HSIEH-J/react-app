e2436c752ad9ac27150d9efb717c68771c7c0f13
YICHEN HSIEH <boisw3641@gmail.com>
2021-09-01 01:53:48 +0800

    fix: update api host

--- src/util/Api.js
+++ src/util/Api.js
@@ -5,7 +5,7 @@ const MySwal = withReactContent(Swal);
 class Api {
   constructor () {
     // this.API_HOST = "https://www.isho.xyz";
-    this.API_HOST = "http://localhost:3000";
+    this.API_HOST = "";
   }
 
   async generateShortUrl (data) {
